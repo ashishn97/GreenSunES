72620db11bd8d5e80fd0f4bc35ea3b85350116f2
NikunjPareek <nikunjpareek25@gmail.com>
2026-05-01 19:33:56 +0530

    change the port

--- quotation/app.js
+++ quotation/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://greensunesbe.onrender.com';
 
 const tableDefaults = [
     { component: 'PV Modules',                              spec: 'BIFICAL/Topcon',                              company: 'Adani (600+ W)',            qty: '10 No.'      },
